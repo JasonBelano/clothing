1faa71d7ec29cc23fbe5d7208199b968a85ddd22
Jason Belano <jasonbelano22@gmail.com>
2024-12-12 21:27:30 +0800

    Updating directory page and create directory components

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import Directory from './components/directory/directory.components';
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      title: 'Hats',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 2,
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      title: 'Jackets',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 3,
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      title: 'Sneakers',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 4,
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      title: 'Womens',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 5,
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      title: 'Mens',
+      subtitle: 'Shop Now'
+    }
+  ]
+
+  return (
+      <Directory categories={categories}/>
+  );
+}
+
+export default App;
